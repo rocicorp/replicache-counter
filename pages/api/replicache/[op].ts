760a4e72427d93bbe6ff38c5821b4ae9afb677cb
Cesar Alaestante <cesara@gmail.com>
2022-11-03 14:07:43 -0700

    remove index.tsx

--- pages/api/replicache/[op].ts
+++ pages/api/replicache/[op].ts
@@ -1,7 +1,18 @@
 import {NextApiRequest, NextApiResponse} from 'next';
-import {handleRequest} from 'replicache-nextjs/lib/backend';
-
+import {
+  createSpace,
+  handleRequest,
+  spaceExists,
+} from 'replicache-nextjs/lib/backend';
+import NextCors from 'nextjs-cors';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const mutators = {
     increment: async (tx: any, delta: any) => {
       const prev = (await tx.get('count')) ?? 0;
@@ -9,5 +20,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       await tx.put('count', next);
     },
   };
+
+  const spaceID = req.query.spaceID as string;
+  if (spaceID) {
+    if (!(await spaceExists(spaceID))) {
+      await createSpace(spaceID);
+    }
+  } else { 
+    res.status(400).send('missing spaceID');
+    return;
+  }
+
   await handleRequest(req, res, mutators);
 };
